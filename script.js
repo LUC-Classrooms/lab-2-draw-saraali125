7319a86efd5c6c53cc64aa72cb50af041a289fe4
saraali125 <157318423+saraali125@users.noreply.github.com>
2024-01-22 17:13:50 +0000

    Added circle

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ function setup(){
 function draw(){
   background(200);
   // add your drawing code here ...
+  fill(255, 200, 150)
+  ellipse(50, 50, 50)
+  ellipse(200, 50, 50)
+  rect(80, 80, 100)
   
 }

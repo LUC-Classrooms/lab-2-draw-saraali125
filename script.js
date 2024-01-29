21b58f9ddba914450af59693305e0671515147fe
saraali125 <157318423+saraali125@users.noreply.github.com>
2024-01-29 02:11:44 +0000

    cat

--- script.js
+++ script.js
@@ -4,10 +4,50 @@ function setup(){
 
 function draw(){
   background(200);
-  // add your drawing code here ...
-  fill(255, 200, 150)
-  ellipse(50, 50, 50)
-  ellipse(200, 50, 50)
-  rect(80, 80, 100)
+  stroke(255, 165, 50)
+  strokeWeight(1.5)
+  ellipse(200, 100, 100, 100); 
+  triangle(150, 89, 150, 40, 170, 60);
+  triangle(250,89,250,40,230,60);
   
+  fill(248)
+  
+  ellipse(220,90,20,20);
+  ellipse(180,90,20,20);
+  fill(255, 180, 50); 
+  fill(100, 210, 100);
+  ellipse(180,90,10,15)
+  ellipse(220,90,10,15)
+  
+  fill(295, 190, 250); 
+  triangle(200,125,190,107,212,107);
+  fill(255, 180, 50); 
+  stroke(150,60,50)
+  strokeWeight(2.5)
+  line(190, 115, 160, 110);
+  line(190, 115, 160, 130);
+  line(190, 115, 160, 120);
+  line(215, 115, 248, 110);
+  line(215, 115, 246, 120);
+  line(215, 115, 240, 127);
+  
+  fill(140,30,70)
+
+  stroke(235, 150, 50)
+  strokeWeight(2)
+  rect(165, 78, 30, 6);
+  rect(205, 78, 30, 6);
+  fill(255, 180, 50); 
+  
+  stroke(150,60,50)
+  line(200,125,200,135);
+  line(200,135,190,142);
+  line(200,135,210,142);
+  line(190,142,180,135);
+  line(210,142,220,135);
+  
+
+  
+
+
 }
